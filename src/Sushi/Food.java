21841d60e5b7ff7a0af76da90474439f63208005
Julia <berrylily.0709@gmail.com>
2017-04-24 18:45:42 -0700

    Implement abstract 'Food' class

---
+++ src/Sushi/Food.java
@@ -0,0 +1,38 @@
+package Sushi;
+
+public class Food {
+
+    protected String name;
+    protected double price;
+
+    // Basic food information
+    public Food(String itemName, int itemPrice) {
+        name = itemName;
+        price = itemPrice;
+    }
+
+    // Set food's name
+    public void setName(String itemName) {
+        name = itemName;
+    }
+
+    // Set food's price
+    public void setNumber(int itemPrice) {
+        price = itemPrice;
+    }
+
+    // Return food's name.
+    public String getName() {
+        return name;
+    }
+
+    // Return food's price.
+    public double getPrice() {
+        return price;
+    }
+
+    // Return description of food as string.
+    public String toString() {
+        return name + "\t" + price;
+    }
+}
